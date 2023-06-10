339b174ba4aca588012ccae1422393defac7b989
Egemen <developer.egemen@gmail.com>
2023-06-10 18:07:41 +0300

    file update

--- ECOMMERCEAPI/index.js
+++ ECOMMERCEAPI/index.js
@@ -1,5 +1,6 @@
 // Import express library
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require("./routes/user");
