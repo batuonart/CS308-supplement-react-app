b19c160b796461cff7b32573dd2a62add1018e24
Altug Yalim <altugyalm@gmail.com>
2023-06-11 03:48:46 +0300

    Added wishlist to the user, and created a list wishlist

--- ECOMMERCEAPI/models/User.js
+++ ECOMMERCEAPI/models/User.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const Product = require("./Product");
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
@@ -7,7 +8,9 @@ const UserSchema = new mongoose.Schema({
     isAdmin: { type: Boolean, default: false }, //Users are not created as admin, defaultly.
     isProductManager: {type: Boolean, default: false},
     isSalesManager: {type: Boolean, default: false},
-    taxId: {type: String}
+    taxId: {type: String},
+    couponPoints: {type:String},
+    wishlist: [{type:Object, unique:true}]
 },
     { timestamps: true } //Auto create timestams
 );
